8e437da0820ebbc8c6bfebd80255bd5b93bb1591
Evgeny Cherkashin <eugene@irnok.net>
2017-11-10 19:24:19 +0800

    Added test of gtk3, libgflow, libgtkflow.

--- icc.modelstudio/src/tests/test_basic.py
+++ icc.modelstudio/src/tests/test_basic.py
@@ -2,6 +2,8 @@ from nose.plugins.skip import SkipTest
 from nose.tools import assert_raises, nottest
 
 #@SkipTest
+
+
 class TestBasic:
 
     def setUp(self):
@@ -9,6 +11,15 @@ class TestBasic:
 
     def test_something(self):
         assert 1 + 1 == 2
+
+    def test_gobject(self):
+        import gi
+        gi.require_version('Gtk', '3.0')
+        from gi.repository import GLib
+        from gi.repository import Gtk
+        #gi.require_version('GFlow', '1.0')
+        from gi.repository import GFlow
+        from gi.repository import GtkFlow
 
     def tearDown(self):
         pass
