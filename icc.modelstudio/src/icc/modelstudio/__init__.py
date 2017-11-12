7d6ba416adaf681b083b8ae596dc3ca4e0cb156c
Evgeny Cherkashin <eugene@irnok.net>
2017-11-12 10:54:39 +0800

    Added requirements and realized the entry point.

--- icc.modelstudio/src/icc/modelstudio/__init__.py
+++ icc.modelstudio/src/icc/modelstudio/__init__.py
@@ -1,5 +1,7 @@
 # Example package with a console entry point
 from __future__ import print_function
 
-def main():
-    print ("Hello World")
+
+def main(*args, **kwargs):
+    from isu.gui.gtk import main
+    main(*args, **kwargs)
