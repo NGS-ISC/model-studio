ef5d5428630967a495b0c3df7771abe957c6fae9
Evgeny Cherkashin <eugene@irnok.net>
2017-11-30 09:48:33 +0800

    Tried to add GI_PATH, failed.

--- icc.modelstudio/src/tests/test_basic.py
+++ icc.modelstudio/src/tests/test_basic.py
@@ -1,5 +1,10 @@
 from nose.plugins.skip import SkipTest
 from nose.tools import assert_raises, nottest
+import os
+
+typelib = '/home/eugeneai/development/codes/ngs/model-studio/vala/builddir'
+
+os.environ['GI_TYPELIB_PATH'] = typelib
 
 #@SkipTest
 
@@ -17,10 +22,12 @@ class TestBasic:
         gi.require_version('Gtk', '3.0')
         gi.require_version('GFlow', '0.2')
         gi.require_version('GtkFlow', '0.2')
+        gi.require_version('ModelStudio', '0.1')
         from gi.repository import GLib
         from gi.repository import Gtk
         from gi.repository import GFlow
         from gi.repository import GtkFlow
+        from gi.repository import ModelStudio
 
     def tearDown(self):
         pass
