22f1a0d0577f868ad650777a465f5da3b7958396
Evgeny Cherkashin <eugene@irnok.net>
2017-12-10 17:42:46 +0800

    Trying to complex the results.

--- icc.modelstudio/src/tests/test_basic.py
+++ icc.modelstudio/src/tests/test_basic.py
@@ -19,23 +19,34 @@ class TestBasic:
         assert 1 + 1 == 2
 
     def test_gobject(self):
+        import os
+        os.environ["GI_TYPELIB_PATH"] = typelib
         import gi
         import pprint
         gi.require_version('GIRepository', '2.0')
         from gi.repository import GIRepository
-        # pprint.pprint(dir(GIRepository.Repository))
-        GIRepository.Repository.prepend_search_path(typelib)
+        Rep = GIRepository.Repository
+        # pprint.pprint(dir(Rep))
+        # print(Rep.get_loaded_namespaces())
+        print(Rep.get_search_path())
+        # GIRepository.Repository.prepend_search_path(typelib)
+        # print(Rep.get_search_path())
+        rep = Rep.get_default()
+        print(rep.get_loaded_namespaces())
+
         gi.require_version('Gtk', '3.0')
         gi.require_version('GFlow', '0.2')
         gi.require_version('GtkFlow', '0.2')
-        gi.require_version('ModelStudio', '0.1')
+        print("1:", rep.get_loaded_namespaces())
         from gi.repository import GLib
         from gi.repository import Gtk
         from gi.repository import GFlow
         from gi.repository import GtkFlow
-        from gi.repository import ModelStudio
+        print("2:", rep.get_loaded_namespaces())
+        gi.require_version('ModelStudio', '0.1')
+        #from gi.repository import ModelStudio
         print(dir(ModelStudio.Application))
-        app = ModelStudio.Application()
+        app = ModelStudio.Application.new()
         app.run()
 
     def tearDown(self):
